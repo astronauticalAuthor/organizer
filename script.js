beb5540a74c8c7a585a9241b7f3a103d64a08459
Katrina Kerrick <katrina.kerrick@sportngin.com>
2014-06-19 20:39:43 -0500

    Initial commit, favicon is done

---
+++ script.js
@@ -0,0 +1,14 @@
+function add() {
+  //adds a row
+  console.log("add")
+}
+
+function remove() {
+  //removes a row. Will not remove a row if there are only nine squares
+  console.log("remove")
+}
+
+function reset() {
+  //resets all squares to default values (empty except for middle), and sets number of squares to nine
+  console.log("reset")
+}
