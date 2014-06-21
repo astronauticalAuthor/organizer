916bb4ccf310d2cd9cf83dba49ec00ab7ea31111
Katrina Kerrick <katrina.kerrick@sportngin.com>
2014-06-20 20:59:36 -0500

    Fixed those forkin' buttons.

---
+++ main.js
@@ -0,0 +1,14 @@
+function addRow() {
+  //adds a row
+  console.log("addRow")
+}
+
+function removeRow() {
+  //removes a row. Will not remove a row if there are only nine squares
+  console.log("removeRow")
+}
+
+function reset() {
+  //resets all squares to default values (empty except for middle), and sets number of squares to nine
+  console.log("reset")
+}
